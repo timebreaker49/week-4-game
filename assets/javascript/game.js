7e8683435f8e8ff0f5c5adf9abe94d477abd7ef3
Wayne Baylor <wcsbaylor@gmail.com>
2018-02-18 20:55:18 -0600

    fixed positioning of images and dropped in characters, started building barebones js

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+(function () {
+    //IIFE logic here 
+})();
+
+ $(document).ready(function() {
+
+var sora = {};
+var cloud = {};
+var mickey = {};
+var sephiroth = {};
+var yourCharacter;
+var enemies-available = [];
+var enemy-defender;
+
+
+$('document').on('click', function() {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+ }
+
